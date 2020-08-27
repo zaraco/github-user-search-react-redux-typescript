5a5c72e120e6bd21a2dd82a9a6f789b5e8f00abb
Zaraco <zahrateymouri90@gmail.com>
2020-08-27 19:10:14 +0200

    api config added

---
+++ src/config.tsx
@@ -0,0 +1,11 @@
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+export const API_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    "X-Requested-With": "XMLHttpRequest"
+};
+
+export const API_ENDPOINTS = {
+    users: `${API_BASE_URL}/users`
+};
