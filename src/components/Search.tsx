c99dee3303dd20db071168d3753ece65f1811894
Zaraco <zahrateymouri90@gmail.com>
2020-08-28 11:36:05 +0200

    search and table style added

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import useUsers from "../hooks/useUsers";
-import {Row, Col, Container} from "react-bootstrap";
+import {Row, Col, Container, Navbar, Form, FormControl, Table} from "react-bootstrap";
 
 
 export default function Search() {
@@ -11,14 +11,69 @@ export default function Search() {
 
     }, []);
 
+    // {JSON.stringify(users)}
+    //  {JSON.stringify(organizations)}
+
     return (
         <>
             <Container>
                 <Row>
-                    <Col md={4}>
+                    <Col md={10}>
+                        <h1>
+                            Search for Github Users
+                        </h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <Navbar bg="dark" expand="lg">
+                            <Form>
+                                <Form.Group controlId="formBasicSearch">
+                                    <FormControl type="text" placeholder="Search"/>
+                                </Form.Group>
+                            </Form>
+                        </Navbar>
+                    </Col>
+                </Row>
 
-                        {JSON.stringify(users)}
-                        {JSON.stringify(organizations)}
+                <Row>
+                    <Col md={6}>
+                        <Table responsive="sm">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                    <Col md={6}>
+                        <Table responsive="sm">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                            </tr>
+                            </tbody>
+                        </Table>
                     </Col>
                 </Row>
             </Container>
