2c7f6b70ff8edea8eeff447cf37226c7b2d2e53e
Zaraco <zahrateymouri90@gmail.com>
2020-08-31 13:38:17 +0200

    style improved

--- src/components/OrganizationsView.tsx
+++ src/components/OrganizationsView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {User} from "../models/User";
 import {Badge, Table} from "react-bootstrap";
 import UserView from "./UserView";
+import './OranizationsView.css';
 
 
 export default function OrganizationsView(props: { organizations: Array<User> }) {
@@ -12,7 +13,7 @@ export default function OrganizationsView(props: { organizations: Array<User> })
 
     return (
         <>
-            organizations {props.organizations.length ? <Badge variant="dark">{props.organizations.length}</Badge> : null}
+            organizations {props.organizations.length ? <Badge variant="dark" className="badge-organs">{props.organizations.length}</Badge> : null}
 
             <Table responsive="sm">
                 <thead>
--- src/components/UsersView.tsx
+++ src/components/UsersView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {User} from "../models/User";
 import {Badge, Table} from "react-bootstrap";
 import UserView from "./UserView";
+import './UsersView.css';
 
 
 export default function UsersView(props: { users: Array<User> }) {
@@ -12,7 +13,7 @@ export default function UsersView(props: { users: Array<User> }) {
 
     return (
         <>
-            users {props.users.length ? <Badge variant="dark">{props.users.length}</Badge> : null}
+            users {props.users.length ? <Badge variant="dark" className="badge-users">{props.users.length}</Badge> : null}
             <Table responsive="sm">
                 <thead>
                 <tr>
