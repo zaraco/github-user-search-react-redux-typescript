178183a288f5bbc280641306e4e51f4e9b810ba2
Zaraco <zahrateymouri90@gmail.com>
2020-09-01 10:37:47 +0200

    UI improved

--- src/components/UsersView.tsx
+++ src/components/UsersView.tsx
@@ -70,11 +70,11 @@ export default function UsersView(props: { users: Array<User>, title: string, ta
                 <tr>
                     <th onClick={sortNameHandler}>
                         {props.tableHeader}
-                        { (submittedSearch.sort === "") ? <FontAwesomeIcon icon={(submittedSearch.order !== "asc") ? faChevronUp : faChevronDown}/> : "" }
+                        { (submittedSearch.sort === "") ? <FontAwesomeIcon style={{cursor:'pointer'}} icon={(submittedSearch.order !== "asc") ? faChevronUp : faChevronDown}/> : "" }
                     </th>
                     <th onClick={sortContributionHandler}>
                         Contributions
-                        { (submittedSearch.sort === "repositories") ? <FontAwesomeIcon icon={(submittedSearch.order !== "asc") ? faChevronUp : faChevronDown}/> : "" }
+                        { (submittedSearch.sort === "repositories") ? <FontAwesomeIcon style={{cursor:'pointer'}} icon={(submittedSearch.order !== "asc") ? faChevronUp : faChevronDown}/> : "" }
                     </th>
                 </tr>
                 </thead>
